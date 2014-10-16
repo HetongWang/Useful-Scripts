97434b4d69f6435c1836c26b6721b7af2e2d1c70
王同和 <Hetong583@qq.com>
2014-10-17 00:54:16 +0800

    beautify the code

--- ZJUlogin.py
+++ ZJUlogin.py
@@ -2,33 +2,21 @@ import os
 import urllib, urllib2
 import ConfigParser
 
-config = ConfigParser.RawConfigParser()
-config.read('E:\GitHub\Python_pro\config.ini')
-username = config.get('zjulogin', 'username')
-password = config.get('zjulogin', 'password')
-url = "https://net.zju.edu.cn/rad_online.php"
-d = {
-    'action': 'auto_dm',
-    'username': username,
-    'password': password
-}
-urllib2.urlopen(urllib2.Request(
-    url,
-    headers = {'Referer': "https://net.zju.edu.cn/srun_port1.php?"},
-    data = urllib.urlencode(d)
-))
+class zjulogin(object):
 
-url = "https://net.zju.edu.cn/cgi-bin/srun_portal"
-d = {
-	'action': 'login',
-	'username': username,
-	'password': password,
-	'ac_id': 3,
-	'type': 1,
-	'wbaredirect': "www.baidu.com"
-}
-print urllib2.urlopen(urllib2.Request(
-	url, 
-	headers = {'Referer': "https://net.zju.edu.cn/srun_port1.php"},
-	data = urllib.urlencode(d)
-)).read()
+    def __init__(self, configFile):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(configFile)
+
+    def post(self, url, item):
+        data = self.config.items(item)
+        url = url
+        print urllib2.urlopen(urllib2.Request(
+            url,
+            headers = {'Referer': "https://net.zju.edu.cn/srun_port1.php?"},
+            data = urllib.urlencode(data)
+        )).read()
+
+login = zjulogin("E:\GitHub\Python_pro\zjulogin.ini")
+login.post("https://net.zju.edu.cn/rad_online.php", "auto_dm")
+login.post("https://net.zju.edu.cn/cgi-bin/srun_portal", "login")
